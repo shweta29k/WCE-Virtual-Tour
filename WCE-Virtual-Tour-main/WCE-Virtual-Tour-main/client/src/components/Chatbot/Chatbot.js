9fab8e41908b1f426b21c927c5902286e9f01cd6
shweta29k <shwetakakade2921@gmail.com>
2024-04-24 20:59:07 +0530

    committed

--- WCE-Virtual-Tour-main/WCE-Virtual-Tour-main/client/src/components/Chatbot/Chatbot.js
+++ WCE-Virtual-Tour-main/WCE-Virtual-Tour-main/client/src/components/Chatbot/Chatbot.js
@@ -11,7 +11,6 @@ const Chatbot = () => {
     script.onload = () => {
       window.botpressWebChat.init({
         composerPlaceholder: "Chat with wce_Virtual_Assistant",
-          botConversationDescription: "WCE_VIRTUAL_TOUR_CHATBOT",
           botId: "0e5daeeb-2974-4c8b-b9d5-e3e9e99ca850",
           hostUrl: "https://cdn.botpress.cloud/webchat/v1",
           messagingUrl: "https://messaging.botpress.cloud",
@@ -20,9 +19,9 @@ const Chatbot = () => {
           lazySocket: true,
           themeName: "prism",
           botName: "wce_Virtual_Assistant",
-          avatarUrl: "https://www.google.com/imgres?q=chatbot&imgurl=https%3A%2F%2Fopenwebsolutions.in%2Fblog%2Fwp-content%2Fuploads%2F2020%2F02%2Fchatbot-4071274_1920.jpg&imgrefurl=https%3A%2F%2Fopenwebsolutions.in%2Fblog%2Fknow-how-chatbot-development-becoming-next-big-thing-in-industry%2F&docid=5AFOsZzq6QzeVM&tbnid=uRUKdp4vveyRoM&vet=12ahUKEwj7zdXr3NWFAxW6yjgGHZOnA68QM3oFCIEBEAA..i&w=1920&h=1217&hcb=2&ved=2ahUKEwj7zdXr3NWFAxW6yjgGHZOnA68QM3oFCIEBEAA",
+          //avatarUrl: "https://www.google.com/imgres?q=chatbot&imgurl=https%3A%2F%2Fopenwebsolutions.in%2Fblog%2Fwp-content%2Fuploads%2F2020%2F02%2Fchatbot-4071274_1920.jpg&imgrefurl=https%3A%2F%2Fopenwebsolutions.in%2Fblog%2Fknow-how-chatbot-development-becoming-next-big-thing-in-industry%2F&docid=5AFOsZzq6QzeVM&tbnid=uRUKdp4vveyRoM&vet=12ahUKEwj7zdXr3NWFAxW6yjgGHZOnA68QM3oFCIEBEAA..i&w=1920&h=1217&hcb=2&ved=2ahUKEwj7zdXr3NWFAxW6yjgGHZOnA68QM3oFCIEBEAA",
           
-          stylesheet: "https://webchat-styler-css.botpress.app/prod/4bbbf36c-aafc-4c56-911a-0148075337e8/v98753/style.css",
+          stylesheet: "https://webchat-styler-css.botpress.app/prod/4bbbf36c-aafc-4c56-911a-0148075337e8/v20255/style.css",
           frontendVersion: "v1",
           useSessionStorage: true,
           enableConversationDeletion: true,
